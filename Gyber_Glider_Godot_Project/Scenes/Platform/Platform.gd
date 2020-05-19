282fd985c26c284fca6566aabfc5c916b3a02526
Foxomat <fox.ket@freenet.de>
2020-05-19 15:02:29 +0200

    collision, snapping and snap indicator done

--- Gyber_Glider_Godot_Project/Scenes/Platform/Platform.gd
+++ Gyber_Glider_Godot_Project/Scenes/Platform/Platform.gd
@@ -5,10 +5,9 @@ extends KinematicBody2D
 onready var screensize = get_viewport().get_size_override()
 onready var xover = (screensize.x - 1080)/2
 onready var yover = screensize.y - 1920
-var raw_mousepos
-var mousepos
 
 var tween_duration = 0.02
+var platform_move_multi = 20
 
 #offsets for the grid; y_offset is variable (platforms fall), thats why we need
 #to remember the initial y offset
@@ -18,6 +17,8 @@ var y_offset = 20
 #if true, this platform is being dragged around at the moment
 onready var dragging: bool = false
 
+#for snap indicator
+signal being_dragged
 #-------------------------------------------------------------------------------
 
 func _process(delta):
@@ -32,19 +33,12 @@ func _process(delta):
 		#yover = screensize.y - 1920 
 		
 		#correct mouse position
-		raw_mousepos = get_global_mouse_position()
-		mousepos = Vector2(raw_mousepos.x - xover, raw_mousepos.y - yover)
+		var raw_mousepos = get_global_mouse_position()
+		var mousepos = Vector2(raw_mousepos.x - xover, raw_mousepos.y - yover)
 		var snap = nearest_snap(mousepos)
-		
-		#print(get_overlapping_areas().size())
-		#position = Vector2(mousepos.x, mousepos.y)
-		move_and_slide(20*(mousepos - position))
-
-
-#func move():
-#	var overlapping = get_overlapping_areas()
-#	for area in overlapping:
-		
+		emit_signal("being_dragged", self)
+				#move the platform
+		move_and_slide(platform_move_multi*(mousepos - position))
 
 
 #transforming coordinates to the nearest position to snap to (snap to grid)
@@ -60,8 +54,9 @@ func nearest_snap(pos):
 func _platform_input_event(viewport, event, shape_idx):
 	if event is InputEventScreenTouch:
 		if event.is_pressed():
+			connect("being_dragged", get_node("/root/Ingame"), "platform_dragged")
+			get_node("/root/Ingame/SnapIndicator").visible = true
 			dragging = true
-
 
 
 #exit dragging mode if finger is released and snap to the grid
@@ -69,6 +64,9 @@ func _input(event):
 	if event is InputEventScreenTouch:
 		if not event.is_pressed():
 			dragging = false
+			if is_connected("being_dragged", get_node("/root/Ingame"), "platform_dragged"):
+				disconnect("being_dragged", get_node("/root/Ingame"), "platform_dragged")
+			get_node("/root/Ingame/SnapIndicator").visible = false
 			$tween.interpolate_property(self, "position", position, 
 					nearest_snap(position), tween_duration, Tween.TRANS_LINEAR)
 			$tween.start()
